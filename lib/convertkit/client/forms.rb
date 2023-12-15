86119e5abe09e3b19ddf5ae116476e79fdeb6b79
Chadwick Wood <chadwick@coffeeshopped.com>
2023-12-15 13:57:57 -0600

    Adding method to get subscriptions to form.

--- lib/convertkit/client/forms.rb
+++ lib/convertkit/client/forms.rb
@@ -13,6 +13,15 @@ module Convertkit
           f.params['tags'] = options[:tags]
         end
       end
+      
+      def subscriptions_to_form(form_id, options = {})
+        connection.get("forms/#{form_id}/subscriptions") do |f|
+          f.params["page"] = options[:page] if options[:page]
+          f.params["sort_order"] = options[:sort_order] if options[:sort_order]
+          f.params["subscriber_state"] = options[:subscriber_state] if options[:subscriber_state]
+        end
+      end
+
     end
   end
 end
